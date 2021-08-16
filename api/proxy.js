0e0786f12552e7f45fbcdc6877bc15bbb591f577
李鹏宇 <lpy_cf@126.com>
2021-08-16 18:45:49 +0800

    serverless

---
+++ api/proxy.js
@@ -0,0 +1,16 @@
+const { createProxyMiddleware } = require( 'http-proxy-middleware' )
+
+module.exports = ( req, res ) => {
+    let target = ''
+
+    // 代理目标地址
+    // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
+    
+    target = 'http://eduboss.lagou.com'
+
+    // 创建代理对象并转发请求
+    createProxyMiddleware( {
+        target,
+        changeOrigin: true,
+    } )( req, res )
+}
